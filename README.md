9478bb779e18516919bc489f64422e426365e520
freewind <nowindlee@gmail.com>
2018-07-14 16:00:58 +0800

    stackpane demo

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-JavaFX Hello World Demo
-=======================
+JavaFX Stack Pane Demo
+======================
 
-Show a window with `Hello, JavaFX` label.
+Layout -> `StackPane`
 
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -2,7 +2,8 @@ package demo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,7 +16,21 @@ public class Hello extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello");
         StackPane root = new StackPane() {{
-            getChildren().add(new Label("Hello, JavaFX!"));
+            getChildren().add(new Pane() {{
+                getChildren().addAll(
+                        new Pane() {{
+                            this.setStyle("-fx-background-color: #AAAAAA");
+                            this.setPrefHeight(200);
+                            this.setPrefWidth(200);
+                        }},
+                        new Pane() {{
+                            this.setStyle("-fx-background-color: RED");
+                            this.setPrefHeight(100);
+                            this.setPrefWidth(100);
+                        }},
+                        new Button("Hello")
+                );
+            }});
         }};
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
